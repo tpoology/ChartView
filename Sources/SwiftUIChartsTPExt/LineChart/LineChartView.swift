1458ee39fd22b000fbcd3519e1873e4fe38483eb
Larry <admin@amazingparrot.com>
2022-03-15 11:03:46 -0600

    Adaptive Renamed Module

--- Sources/SwiftUIChartsTPExt/LineChart/LineChartView.swift
+++ Sources/SwiftUIChartsTPExt/LineChart/LineChartView.swift
@@ -15,11 +15,11 @@ public struct LineChartView: View {
     public var legend: String?
     public var style: ChartStyle
     public var darkModeStyle: ChartStyle
-    
+
     public var formSize:CGSize
     public var dropShadow: Bool
     public var valueSpecifier:String
-    
+
     @State private var touchLocation:CGPoint = .zero
     @State private var showIndicatorDot: Bool = false
     @State private var currentValue: Double = 2 {
@@ -27,21 +27,21 @@ public struct LineChartView: View {
             if (oldValue != self.currentValue && showIndicatorDot) {
                 HapticFeedback.playSelection()
             }
-            
+
         }
     }
     var frame = CGSize(width: 180, height: 120)
     private var rateValue: Int?
-    
+
     public init(data: [Double],
                 title: String,
                 legend: String? = nil,
                 style: ChartStyle = Styles.lineChartStyleOne,
                 form: CGSize? = ChartForm.medium,
-                rateValue: Int?,
+                rateValue: Int? = 2,
                 dropShadow: Bool? = true,
                 valueSpecifier: String? = "%.1f") {
-        
+
         self.data = ChartData(points: data)
         self.title = title
         self.legend = legend
@@ -53,7 +53,7 @@ public struct LineChartView: View {
         self.valueSpecifier = valueSpecifier!
         self.rateValue = rateValue
     }
-    
+
     public var body: some View {
         ZStack(alignment: .center){
             RoundedRectangle(cornerRadius: 20)
@@ -73,7 +73,7 @@ public struct LineChartView: View {
                                 .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor :self.style.legendTextColor)
                         }
                         HStack {
-                            
+
                             if let rateValue = self.rateValue
                             {
                                 if (rateValue ?? 0 >= 0){
@@ -81,7 +81,7 @@ public struct LineChartView: View {
                                 }else{
                                     Image(systemName: "arrow.down")
                                 }
-                                Text("\(rateValue!)%")
+                                Text("\(rateValue)%")
                             }
                         }
                     }
@@ -124,12 +124,12 @@ public struct LineChartView: View {
             })
         )
     }
-    
+
     @discardableResult func getClosestDataPoint(toPoint: CGPoint, width:CGFloat, height: CGFloat) -> CGPoint {
         let points = self.data.onlyPoints()
         let stepWidth: CGFloat = width / CGFloat(points.count-1)
         let stepHeight: CGFloat = height / CGFloat(points.max()! + points.min()!)
-        
+
         let index:Int = Int(round((toPoint.x)/stepWidth))
         if (index >= 0 && index < points.count){
             self.currentValue = points[index]
@@ -144,7 +144,7 @@ struct WidgetView_Previews: PreviewProvider {
         Group {
             LineChartView(data: [8,23,54,32,12,37,7,23,43], title: "Line chart", legend: "Basic")
                 .environment(\.colorScheme, .light)
-            
+
             LineChartView(data: [282.502, 284.495, 283.51, 285.019, 285.197, 286.118, 288.737, 288.455, 289.391, 287.691, 285.878, 286.46, 286.252, 284.652, 284.129, 284.188], title: "Line chart", legend: "Basic")
             .environment(\.colorScheme, .light)
         }
